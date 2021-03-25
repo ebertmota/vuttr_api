2c82715d738edbc43178b842987a7e8e9aaba6d4
ebertmota <ebert.mota03@gmail.com>
2021-03-24 23:22:02 -0300

    eslint auto changin commas

--- src/database/migrations/1616546313212-CreateTools.ts
+++ src/database/migrations/1616546313212-CreateTools.ts
@@ -1,40 +1,40 @@
-import { MigrationInterface, QueryRunner, Table } from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export default class CreateTools1616546313212 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: "tools",
+        name: 'tools',
         columns: [
           {
-            name: "id",
-            type: "varchar",
+            name: 'id',
+            type: 'varchar',
             isPrimary: true,
-            generationStrategy: "uuid",
-            default: "uuid_generate_v4()",
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
           },
           {
-            name: "title",
-            type: "varchar",
+            name: 'title',
+            type: 'varchar',
           },
           {
-            name: "link",
-            type: "varchar",
+            name: 'link',
+            type: 'varchar',
           },
           {
-            name: "description",
-            type: "varchar",
+            name: 'description',
+            type: 'varchar',
           },
           {
-            name: "tags",
-            type: "varchar",
+            name: 'tags',
+            type: 'varchar',
           },
         ],
-      })
+      }),
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable("tools");
+    await queryRunner.dropTable('tools');
   }
 }
