043448eee539a1e590efcccf0e576cf0f546f51a
ebertmota <ebert.mota03@gmail.com>
2021-03-25 22:37:47 -0300

    create globalExceptionHandler middleware

---
+++ src/middlewares/globalExceptionHandler.ts
@@ -0,0 +1,25 @@
+import { Request, Response, NextFunction } from 'express';
+
+import AppError from '../errors/AppError';
+
+export default function globalExceptionHandler(
+  err: Error,
+  resquest: Request,
+  response: Response,
+  _: NextFunction,
+): Response {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  // eslint-disable-next-line
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+}
