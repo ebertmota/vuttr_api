c03aa35f35245de05f78b51b7b93b6abe36a71c9
ebertmota <ebert.mota03@gmail.com>
2021-03-25 22:35:47 -0300

    sharing responsibilities with Controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,6 +3,10 @@ import toolsRouter from './tools.routes';
 
 const routes = Router();
 
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Hello VUTTR lives!' });
+});
+
 routes.use('/tools', toolsRouter);
 
 export default routes;
--- src/routes/tools.routes.ts
+++ src/routes/tools.routes.ts
@@ -1,74 +1,15 @@
 import { Router } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { validate } from 'uuid';
 
-import CreateToolService from '../services/CreateToolService';
-import ToolsRepository from '../repositories/ToolsRepository';
+import ToolsController from '../controllers/ToolsController';
 
 const toolsRouter = Router();
-const createToolService = new CreateToolService();
+const toolsController = new ToolsController();
 
 // List all tools, or list tools based on tags passed by query params
-toolsRouter.get('/', async (request, response) => {
-  const toolsRepository = getCustomRepository(ToolsRepository);
+toolsRouter.get('/', toolsController.index);
 
-  const { tags } = request.query;
+toolsRouter.post('/', toolsController.create);
 
-  let tools;
-
-  tools = await toolsRepository.find();
-
-  if (tags !== undefined) {
-    console.log(tags);
-
-    tools = await toolsRepository.findByTags(tags);
-  }
-
-  return response.json(tools);
-});
-
-// Create Tool
-toolsRouter.post('/', async (request, response) => {
-  const { title, link, description, tags } = request.body;
-
-  const tool = await createToolService.execute({
-    title,
-    link,
-    description,
-    tags,
-  });
-
-  return response.status(201).json(tool);
-});
-
-// Delete Tool By ID
-
-toolsRouter.delete('/:id', async (request, response) => {
-  const toolsRepository = getCustomRepository(ToolsRepository);
-
-  const { id } = request.params;
-
-  // Check if id is a uuid
-  if (!validate(id)) {
-    return response.status(404).json({ error: 'Invalid type of id' });
-  }
-
-  // Searching in database for tool
-  const tool = await toolsRepository.find({
-    where: { id },
-  });
-
-  // Checking if tools was found
-  if (tool.length === 0) {
-    return response
-      .status(404)
-      .json({ error: 'Tool with provided Id was not found.' });
-  }
-
-  await toolsRepository.delete(id);
-  console.log(tool);
-
-  return response.status(204).json();
-});
+toolsRouter.delete('/:id', toolsController.delete);
 
 export default toolsRouter;
